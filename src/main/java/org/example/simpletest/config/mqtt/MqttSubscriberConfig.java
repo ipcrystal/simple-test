cc2c0c1225ab67fc3ac77116cba5f5fde4061d24
winterfell <mimotronik@gmail.com>
2020-12-18 00:27:23 +0800

    配置文件名称修改

--- src/main/java/org/example/simpletest/config/mqtt/MqttSubscriberConfig.java
+++ src/main/java/org/example/simpletest/config/mqtt/MqttSubscriberConfig.java
@@ -1,8 +1,10 @@
 package org.example.simpletest.config.mqtt;
 
 /**
+ * mqtt 订阅的配置
+ *
  * @author zhuzhenjie
  * @since 2020/12/10
  */
-public class MqttSusbcriberConfig {
+public class MqttSubscriberConfig {
 }
