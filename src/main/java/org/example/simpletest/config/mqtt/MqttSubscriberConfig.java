95eabc91195563a4232ecf0f805eeb8004b822cd
zhuzhenjie <mimotronik@gmail.com>
2021-01-26 16:21:28 +0800

    mqtt 消费

--- src/main/java/org/example/simpletest/config/mqtt/MqttSubscriberConfig.java
+++ src/main/java/org/example/simpletest/config/mqtt/MqttSubscriberConfig.java
@@ -1,10 +1,103 @@
 package org.example.simpletest.config.mqtt;
 
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
 /**
  * mqtt 订阅的配置
  *
  * @author zhuzhenjie
  * @since 2020/12/10
  */
+@Configuration
+@IntegrationComponentScan
+@Slf4j
 public class MqttSubscriberConfig {
+
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Value("${mqtt.broker}")
+    private String broker;
+
+    @Value("${mqtt.subscriber.clientId}")
+    private String clientId;
+
+    @Value("${mqtt.subscriber.defaultTopic}")
+    private String defaultTopic;
+
+
+    @Bean
+    public MqttConnectOptions subscriberMqttConnectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        options.setServerURIs(new String[]{broker});
+        options.setKeepAliveInterval(2);
+        return options;
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setConnectionOptions(subscriberMqttConnectOptions());
+        return factory;
+    }
+
+    /**
+     * 接收通道
+     *
+     * @return
+     */
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    /**
+     * 配置client，监听topic
+     *
+     * @return
+     */
+    @Bean
+    public MessageProducer inbound() {
+        // topic 订阅的地方
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(clientId, mqttClientFactory(), defaultTopic);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            String topic = String.valueOf(message.getHeaders().get("mqtt_receivedTopic"));
+            String payload = message.getPayload().toString();
+
+            log.info("--------------------START-------------------");
+            log.info("接收到订阅消息:");
+            log.info("topic:{}", topic);
+            log.info("message:{}", payload);
+            log.info("---------------------END--------------------");
+        };
+    }
 }
